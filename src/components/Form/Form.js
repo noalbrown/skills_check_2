d381965e711d5bb93c2398f3b4e59f9174b5b4b0
Noal Brown <noalbrown1@gmail.com>
2020-08-07 12:01:35 -0500

    skills2

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  constructor() {
+    super()
+    state = {
+      imageURL: '',
+      productName: '',
+      price: 0
+    }
+  }
+
+  handleImageURL(e) {
+    this.setState({
+      imageURL: e.target.value
+    })
+  }
+
+  handleProductName(e) {
+    this.setState({
+      productName: e.target.value
+    })
+  }
+
+  handlePrice(e) {
+    this.setState({
+      price: e.target.value
+    })
+  }
+
+  handleReset() {
+    this.setState({
+      imageURL: '', productName: '', productName: 0
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>Form</h1>
+        <h3>Image URL:</h3>
+        <input onChange={(e) => {
+          this.handleImageURL(e)
+        }} />
+
+        <h3>Product Name:</h3>
+        <input onChange={(e) => {
+          this.handleProductName(e)
+        }} />
+
+        <h3>Price:</h3>
+        <input onChange={(e) => {
+          this.handlePrice(e)
+        }} />
+        <div>
+          <button onClick={(e) => this.handleReset(e)}>Cancel</button>
+          <button>Add to inventory</button>
+        </div>
       </div>
     );
   }
